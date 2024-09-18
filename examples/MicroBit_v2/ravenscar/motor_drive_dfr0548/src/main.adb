2a5119c7f8548873c35b044008e09902f03b3028
aiunderstand <steven@stevenbos.com>
2024-09-18 12:00:56 +0200

    Added .vscode folder to each of the Ravenscar examples of Microbit_V2

--- examples/MicroBit_v2/ravenscar/motor_drive_dfr0548/src/main.adb
+++ examples/MicroBit_v2/ravenscar/motor_drive_dfr0548/src/main.adb
@@ -44,12 +44,16 @@ begin
    loop
       -- DEMONSTRATION ROUTINE 4 MOTORS (useful for checking your wiring)
       MotorDriver.Drive(Forward,(4095,0,0,0)); --right front wheel to M4
+      Console.Put_Line("RF");
       delay 1.0;
       MotorDriver.Drive(Forward,(0,4095,0,0)); --right back wheel to M3
+      Console.Put_Line("RB");
       delay 1.0;
       MotorDriver.Drive(Forward,(0,0,4095,0)); --left front wheel to M2
+      Console.Put_Line("LF");
       delay 1.0;
       MotorDriver.Drive(Forward,(0,0,0,4095)); --left back wheel to M1
+      Console.Put_Line("LB");
       delay 1.0;
       MotorDriver.Drive(Stop);
 
